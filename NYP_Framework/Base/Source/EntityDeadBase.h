eb5eacb63bfe49b18baf40ce05548a946aa5537f
Wilson <neoh.wilson@gmail.com>
2017-08-15 17:31:11 +0800

    Add blank EntityDead and EntityLiving Header and cpp files

---
+++ NYP_Framework/Base/Source/EntityDeadBase.h
@@ -0,0 +1,9 @@
+#ifndef _ENTITY_DEAD_BASE_H_
+#define _ENTITY_DEAD_BASE_H_
+
+class EntityDeadBase
+{
+
+};
+
+#endif
---
+++ NYP_Framework/Base/Source/EntityLivingBase.h
@@ -0,0 +1,9 @@
+#ifndef _ENTITY_LIVING_BASE_H_
+#define _ENTITY_LIVING_BASE_H_
+
+class EntityLivingBase
+{
+
+};
+
+#endif
